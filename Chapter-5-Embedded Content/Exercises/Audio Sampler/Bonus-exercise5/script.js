81615c0af6cda4c379b838a362dab46ef7d13c14
Muhammad Ali <alioraf789@gmail.com>
2025-04-18 05:32:15 +0400

    Completed mandatory exercise of chapter-6

--- Chapter-5-Embedded Content/Exercises/Audio Sampler/Bonus-exercise5/script.js
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler/Bonus-exercise5/script.js
@@ -1,27 +1,36 @@
-const sounds = ['ah ha', 'back of the net', 'bang out of order', 'dan', 'email of the evening', 
-'hello partridge', 'i ate a scotch egg', 'im confused']
-
-sounds.forEach(sound => {
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
-
-    btn.innerText = sound
-
-    btn.addEventListener('click', () => {
-        stopSongs()
-        document.getElementById(sound).play()
-    })
-
-    document.getElementById('buttons').
-    appendChild(btn)
-})
-
-function stopSongs() {
+const sounds = ['Ah Ha', 'Back of the net', 'Bang out of order', 'Dan', 'Email of the evening', 
+    'Hello Partridge', 'I ate a scotch egg', 'Im confused', 'Jurassic Park'];
+    
     sounds.forEach(sound => {
-        const song = document.getElementById
-        (sound)
-        
-        song.pause()
-        song.currentTime = 0;
-    })
-}
+        const btn = document.createElement('button');
+        btn.classList.add('btn');
+    
+        btn.innerText = sound;
+    
+        btn.addEventListener('click', () => {
+            stopSongs();
+            document.getElementById(sound).play()
+        });
+    
+        document.getElementById('buttons').appendChild(btn);
+    });
+    
+    function stopSongs() {
+        sounds.forEach(sound => {
+            const song = document.getElementById(sound);
+            
+            song.pause()
+            song.currentTime = 0;
+        });
+    }
+  
+    function textToAudio() {
+        let msg = document.getElementById("text-to-speech").value;
+        let speech = new SpeechSynthesisUtterance();
+        speech.lang = "en-US";
+        speech.text = msg;
+        speech.volume = 1;
+        speech.rate = 1;
+        speech.pitch = 1;
+        window.speechSynthesis.speak(speech)
+    }
---
+++ Chapter-6-Javascript/Exercises/Mandatory-exercise6/script.js
@@ -0,0 +1,19 @@
+
+const costInput = document.getElementById('costinfo');
+const litersInput = document.getElementById('literinfo');
+const calculatebtn = document.getElementById('calculatebtn');
+const totalcost = document.getElementById('totalcost');
+
+
+calculatebtn.addEventListener('click', (e)=> {
+
+  const costinfo = parseFloat(costInput.value);
+  const liters = parseFloat(litersInput.value);
+
+
+  if (!isNaN(costinfo) && !isNaN(liters)) {
+    const total = costinfo * liters;
+    totalcost.textContent = `Total cost: $${total}`;
+  }
+});
+
